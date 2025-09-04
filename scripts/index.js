2f94d1f4d655811f765a4a9fab120231f6e52ee9
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-09-04 18:38:04 +0600

    card and lesson section update

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,34 @@ const loadLavelWords = (id) => {
 const displayLevalWords = (words) => {
     const wordsContainer = document.getElementById("words-container");
     wordsContainer.innerHTML = "";
+    // ----------- empty lesson massege ------------
+    if (words.length == 0) {
+        wordsContainer.innerHTML = `
+                <div class="random-text text-center col-span-3">
+                    <i class="fa-solid fa-triangle-exclamation text-[100px] text-red-500 pb-3"></i>
+                      <p class=" text-sm text-gray-400 font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                     <h1 class="text-4xl font-bold pt-3 font-bangla">নেক্সট Lesson এ যান</h1>
+                </div>
+        `;
+        return
+    }
+
+
     words.forEach(word => {
-        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+                <div class="card bg-[#fff] text-center py-10 px-5 w-full shadow-xl">
+                    <h1 class="text-2xl font-bold  ">${word.word}</h1>
+                    <p class="text-sm py-3">meaning/Pronounciation</p>
+                    <h2 class="text-2xl font-semibold ">${word.meaning}/${word.pronunciation}</h2>
+                    <div class="icons text-2xl flex justify-between items-center mt-14">
+                        <a href="" class="bg-[#E8F4FF] p-2 rounded-md"><i class="fa-solid fa-circle-info"></i></a>
+                        <a href="#" class=" bg-[#E8F4FF] p-2 rounded-md"> <i class="fa-solid fa-volume-high"></i></a>
+
+                    </div>
+                </div>
+        `;
+        wordsContainer.appendChild(card)
     })
 }
 
